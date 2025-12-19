e4121796f3034929628c1a8e4d9028a29c121f24
Daecheon Yang <bigskyyang@gmail.com>
2025-12-19 17:47:06 +0900

    feat: [front]회원가입 기능 구현 (UI, 유효성 검사, 중복 확인 API 연동)

--- front/src/router/index.js
+++ front/src/router/index.js
@@ -1,5 +1,8 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import SignUpView from '@/views/SignUpView.vue'
+import LogInView from '@/views/LogInView.vue'
+import AssetView from '@/views/AssetView.vue' 
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,12 +13,19 @@ const router = createRouter({
       component: HomeView,
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue'),
+      path: '/signup',
+      name: 'signup',
+      component: SignUpView
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: LogInView
+    },
+    {
+      path: '/asset',
+      name: 'asset',
+      component: AssetView
     },
   ],
 })
