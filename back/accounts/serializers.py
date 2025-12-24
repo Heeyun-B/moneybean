d76b948e480d59b62a7e1e667bba4a6238f9486e
jameschoi74 <james200074@naver.com>
2025-12-24 10:08:38 +0900

    feat: 프로필 백엔드

--- back/accounts/serializers.py
+++ back/accounts/serializers.py
@@ -44,3 +44,79 @@ class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ('id', 'username', 'email', 'nickname', 'is_staff')
+
+class DepositSubscriptionSerializer(serializers.ModelSerializer):
+    """가입한 예금 상품"""
+    product_name = serializers.CharField(source='product.fin_prdt_nm', read_only=True)
+    bank_name = serializers.CharField(source='product.kor_co_nm', read_only=True)
+    product_code = serializers.CharField(source='product.fin_prdt_cd', read_only=True)
+    interest_rate = serializers.FloatField(source='selected_option.intr_rate', read_only=True)
+    save_term = serializers.IntegerField(source='selected_option.save_trm', read_only=True)
+    
+    class Meta:
+        from deposits.models import DepositSubscription
+        model = DepositSubscription
+        fields = ('id', 'product_code', 'product_name', 'bank_name', 
+                  'interest_rate', 'save_term', 'subscribed_at')
+
+
+class SavingSubscriptionSerializer(serializers.ModelSerializer):
+    """가입한 적금 상품"""
+    product_name = serializers.CharField(source='product.fin_prdt_nm', read_only=True)
+    bank_name = serializers.CharField(source='product.kor_co_nm', read_only=True)
+    product_code = serializers.CharField(source='product.fin_prdt_cd', read_only=True)
+    interest_rate = serializers.FloatField(source='selected_option.intr_rate', read_only=True)
+    save_term = serializers.IntegerField(source='selected_option.save_trm', read_only=True)
+    
+    class Meta:
+        from deposits.models import SavingSubscription
+        model = SavingSubscription
+        fields = ('id', 'product_code', 'product_name', 'bank_name', 
+                  'interest_rate', 'save_term', 'subscribed_at')
+        
+class UserProfileSerializer(serializers.ModelSerializer):
+    """프로필 조회용 Serializer"""
+    deposit_subscriptions = DepositSubscriptionSerializer(many=True, read_only=True)
+    saving_subscriptions = serializers.SerializerMethodField()
+
+    def get_saving_subscriptions(self, obj):
+        from deposits.models import SavingSubscription
+        subscriptions = SavingSubscription.objects.filter(user=obj)
+        return SavingSubscriptionSerializer(subscriptions, many=True).data
+    
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'email', 'nickname', 'first_name', 'last_name', 'date_joined', 'deposit_subscriptions', 'saving_subscriptions')
+        read_only_fields = ('id', 'username', 'date_joined')
+
+
+class UserProfileUpdateSerializer(serializers.ModelSerializer):
+    """프로필 수정용 Serializer"""
+    class Meta:
+        model = User
+        fields = ('email', 'nickname', 'first_name', 'last_name')
+    
+    def validate_nickname(self, value):
+        """닉네임 중복 체크 (자기 자신 제외)"""
+        user = self.context['request'].user
+        if User.objects.exclude(pk=user.pk).filter(nickname=value).exists():
+            raise serializers.ValidationError("이미 사용 중인 닉네임입니다.")
+        return value
+
+
+class ChangePasswordSerializer(serializers.Serializer):
+    """비밀번호 변경용 Serializer"""
+    old_password = serializers.CharField(required=True, write_only=True)
+    new_password = serializers.CharField(required=True, write_only=True, validators=[validate_password])
+    new_password_confirm = serializers.CharField(required=True, write_only=True)
+    
+    def validate_old_password(self, value):
+        user = self.context['request'].user
+        if not user.check_password(value):
+            raise serializers.ValidationError("현재 비밀번호가 일치하지 않습니다.")
+        return value
+    
+    def validate(self, data):
+        if data['new_password'] != data['new_password_confirm']:
+            raise serializers.ValidationError({"new_password_confirm": "새 비밀번호가 일치하지 않습니다."})
+        return data
--- back/accounts/views.py
+++ back/accounts/views.py
@@ -4,7 +4,7 @@ from rest_framework.response import Response
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.authtoken.models import Token
 from django.contrib.auth import authenticate
-from .serializers import SignupSerializer, LoginSerializer, UserSerializer
+from .serializers import SignupSerializer, LoginSerializer, UserSerializer, UserProfileSerializer, UserProfileUpdateSerializer, ChangePasswordSerializer
 from django.contrib.auth import get_user_model
 
 @api_view(['POST'])
@@ -97,3 +97,58 @@ def check_nickname(request, nickname):
     if User.objects.filter(nickname=nickname).exists():
         return Response({'is_exist': True}, status=status.HTTP_200_OK)
     return Response({'is_exist': False}, status=status.HTTP_200_OK)
+
+
+# 프로필 관련 함수
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def profile(request):
+    """로그인한 사용자의 프로필 조회"""
+    serializer = UserProfileSerializer(request.user)
+    return Response(serializer.data)
+
+
+@api_view(['PUT', 'PATCH'])
+@permission_classes([IsAuthenticated])
+def profile_update(request):
+    """로그인한 사용자의 프로필 수정"""
+    serializer = UserProfileUpdateSerializer(
+        request.user, 
+        data=request.data, 
+        partial=True,
+        context={'request': request}  # 닉네임 중복 체크를 위해 필요
+    )
+    
+    if serializer.is_valid(raise_exception=True):
+        serializer.save()
+        return Response(serializer.data)
+
+
+@api_view(['PUT'])
+@permission_classes([IsAuthenticated])
+def change_password(request):
+    """비밀번호 변경"""
+    serializer = ChangePasswordSerializer(
+        data=request.data,
+        context={'request': request}
+    )
+    
+    if serializer.is_valid(raise_exception=True):
+        user = request.user
+        user.set_password(serializer.validated_data['new_password'])
+        user.save()
+        return Response({
+            'message': '비밀번호가 성공적으로 변경되었습니다.'
+        })
+
+
+@api_view(['DELETE'])
+@permission_classes([IsAuthenticated])
+def profile_delete(request):
+    """회원 탈퇴"""
+    user = request.user
+    user.delete()
+    return Response({
+        'message': '회원 탈퇴가 완료되었습니다.'
+    }, status=status.HTTP_204_NO_CONTENT)
