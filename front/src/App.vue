99ca273e34bb0586b32631ab69a67182a63fed2d
jameschoi74 <james200074@naver.com>
2025-12-24 20:48:27 +0900

    fix: 폰트 오류 해결

--- front/src/App.vue
+++ front/src/App.vue
@@ -14,19 +14,34 @@ import TheNavbar from '@/components/common/TheNavbar.vue'
 </script>
 
 <style>
-@import url('https://webfontworld.github.io/gmarket/GmarketSans.css');
+/* 1. 외부 주소 대신 로컬 파일을 불러오도록 설정 */
+@font-face {
+  font-family: 'GmarketSans';
+  src: url('./assets/fonts/GmarketSansTTFMedium.ttf') format('truetype');
+  font-weight: 500;
+  font-style: normal;
+}
 
 * { 
-  /* 지마켓 산스를 최우선으로 하되, 로딩 실패시만 기본 고딕 사용 */
-  font-family: 'GmarketSans', sans-serif; 
-  margin: 0; padding: 0; box-sizing: border-box; 
+  /* 2. 폰트 적용 ( !important를 추가해서 확실히 적용되게 합니다 ) */
+  font-family: 'GmarketSans', sans-serif !important; 
+  margin: 0; 
+  padding: 0; 
+  box-sizing: border-box; 
 }
 
 /* 모바일에서 텍스트가 너무 작아 보이지 않게 방지 */
 html { -webkit-text-size-adjust: 100%; }
 
-body { background-color: #f8faf9; color: #333; -webkit-font-smoothing: antialiased; }
+body { 
+  background-color: #f8faf9; 
+  color: #333; 
+  -webkit-font-smoothing: antialiased; 
+}
 
-/* 모바일 버튼 터치 시 생기는 하이라이트 제거 (선택사항) */
-button { -webkit-tap-highlight-color: transparent; font-family: 'GmarketSans', sans-serif; }
+/* 버튼에도 폰트 적용 */
+button { 
+  -webkit-tap-highlight-color: transparent; 
+  font-family: 'GmarketSans', sans-serif !important; 
+}
 </style>
